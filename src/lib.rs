7090bd9e3d778cf8287d9b1a7ab519d5ff101646
Mara Bos <m-ou.se@m-ou.se>
2018-09-23 12:21:07 +0200

    Work around old version of rustc on docs.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@
 //! They are useful in all places where you would otherwise use
 //! `Vec<u8>` and `[u8]` to represent your strings.
 
+// TODO: Remove this once docs.rs supports rust stable 1.28 or later.
+#![cfg_attr(feature="old-nightly",feature(slice_get_slice))]
+
 mod str;
 mod string;
 
